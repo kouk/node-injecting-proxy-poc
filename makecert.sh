b8d3bc4283bb1a58354b8c932f424222d0fdb9d3
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-10-05 17:18:14 +0300

    update makecert.sh to not clobber the demoCA files

--- makecert.sh
+++ makecert.sh
@@ -5,9 +5,17 @@ CN=${1:-proxy.livelocal}
 if ! [ -f certificate.pem ] ; then
     mkdir -p demoCA/private demoCA/newcerts
     touch demoCA/index.txt
-    echo "01" > demoCA/serial
-    openssl req -x509 -new -newkey rsa:2048 -out demoCA/cacert.pem -keyout demoCA/private/cakey.pem -nodes -batch
+    SERIAL="01"
+    if ! [ -f demoCA/serial ]; then
+        echo "$SERIAL" > demoCA/serial
+    else
+        SERIAL=$(cat demoCA/serial)
+    fi
+    if ! [ -f demoCA/private/cakey.pem ] ; then
+        openssl req -x509 -new -newkey rsa:2048 -out demoCA/cacert.pem -keyout demoCA/private/cakey.pem -nodes -batch
+    fi
     openssl req -new -subj "/CN=$CN" -newkey rsa:2048 -out req.pem -keyout key.pem -nodes -reqexts v3_req_livelocal -config openssl.cnf -nodes -batch
     openssl ca -in req.pem -config openssl.cnf -extensions v3_req_livelocal -policy policy_anything -batch
-    cp demoCA/newcerts/01.pem certificate.pem
+    cp demoCA/newcerts/${SERIAL}.pem certificate.pem
 fi
+
