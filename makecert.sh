f363df47427d45f0be739d40ca110ec73a9948cd
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-10-06 10:14:14 +0300

    enable creating certificates based on env vars in makecert.sh

--- makecert.sh
+++ makecert.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-CN=${1:-proxy.livelocal}
+PROXY_SUFFIX=${1:-livelocal}
+PROXY_CN=${2:-proxy.$PROXY_SUFFIX}
+export PROXY_SUFFIX PROXY_CN
 
 if ! [ -f certificate.pem ] ; then
     mkdir -p demoCA/private demoCA/newcerts
@@ -14,7 +16,7 @@ if ! [ -f certificate.pem ] ; then
     if ! [ -f demoCA/private/cakey.pem ] ; then
         openssl req -x509 -new -newkey rsa:2048 -out demoCA/cacert.pem -keyout demoCA/private/cakey.pem -nodes -batch
     fi
-    openssl req -new -subj "/CN=$CN" -newkey rsa:2048 -out req.pem -keyout key.pem -nodes -reqexts v3_req_livelocal -config openssl.cnf -nodes -batch
+    openssl req -new -subj "/CN=$PROXY_CN" -newkey rsa:2048 -out req.pem -keyout key.pem -nodes -reqexts v3_req_livelocal -config openssl.cnf -nodes -batch
     openssl ca -in req.pem -config openssl.cnf -extensions v3_req_livelocal -policy policy_anything -batch
     cp demoCA/newcerts/${SERIAL}.pem certificate.pem
 fi
