d110b9874a5d0fefc72d56458aa489e61feb6ae7
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-11-10 19:05:35 +0200

    make sure injection happens also on redirect

--- test/inject.js
+++ test/inject.js
@@ -32,5 +32,30 @@ describe('app-inject', function(){
       });
     });
   });
+  it('should inject a simple string on a redirect', function(done){
+    conf.set('inject', [{
+        'select': 'head',
+        'handle_redirect': true,
+        'payload': _.template('<div>FOOBAR</div>')
+    }]);
+    var target = 'target.doesn.t.exist.com:1234',
+        server = proxy(conf),
+        couchdb = nock('http://' + target)
+           .defaultReplyHeaders({
+              'Content-Type': 'text/html',
+              'Location': '/foobar'
+           })
+           .get('/')
+           .reply(301, "<html><head><title>yo</title></head><body>man</body></html>");
+    server.listen(0, function(){
+      request(server)
+      .get('/')
+      .set('Host', base32.encode(target) + '-http' + conf.get('suffix'))
+      .expect(200, function(req, res) {
+          res.text.should.equal('<html><head><title>yo</title><div>FOOBAR</div></head><body>man</body></html>');
+          done();
+      });
+    });
+  });
 });
 
