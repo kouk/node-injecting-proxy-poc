3bf61093186089ec12bc751c023efaa38c254e2f
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-11-17 13:21:21 +0200

    make makecert.sh more usefull for more cases

--- scripts/makecert.sh
+++ scripts/makecert.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+CADIR=demoCA
 if [ $# -eq 0 ] ; then
     echo "Usage: $0 host.domain"
     echo ""
@@ -21,24 +22,34 @@ fi
 
 if [ -n "$REQUESTED_HOSTNAME" ] ; then
     REQUESTED_DOMAIN=.$REQUESTED_DOMAIN
+    reqext=v3_req
+else
+    reqext=v3_req_simple
 fi
 export REQUESTED_HOSTNAME REQUESTED_DOMAIN REQUESTED_CA_CN
 
-if ! [ -f certificate.pem ] ; then
-    mkdir -p demoCA/private demoCA/newcerts
-    touch demoCA/index.txt
+if ! [ -f key.pem ] ; then
+    mkdir -p $CADIR/private $CADIR/newcerts
+    touch $CADIR/index.txt
     SERIAL="01"
-    if ! [ -f demoCA/serial ]; then
-        echo "$SERIAL" > demoCA/serial
+    if ! [ -f $CADIR/serial ]; then
+        echo "$SERIAL" > $CADIR/serial
     else
-        SERIAL=$(cat demoCA/serial)
+        SERIAL=$(cat $CADIR/serial)
     fi
-    if ! [ -f demoCA/private/cakey.pem ] ; then
+    if ! [ -f $CADIR/private/cakey.pem ] ; then
         echo "Creating a new CA certificate.."
-        openssl req -subj "/CN=$REQUESTED_CA_CN" -x509 -new -newkey rsa:2048 -out demoCA/cacert.pem -keyout demoCA/private/cakey.pem -nodes -batch -config openssl.cnf
+        openssl req -subj "/CN=$REQUESTED_CA_CN" -x509 -new -newkey rsa:2048 -out $CADIR/cacert.pem -keyout $CADIR/private/cakey.pem -nodes -batch -config openssl.cnf
     fi
-    openssl req -new -newkey rsa:2048 -out req.pem -keyout key.pem -nodes -config openssl.cnf -nodes -batch
-    openssl ca -noemailDN -in req.pem -config openssl.cnf -extensions v3_req -policy policy_anything -batch
-    cp demoCA/newcerts/${SERIAL}.pem certificate.pem
+    privkey=$CADIR/private/${SERIAL}.pem
+    if [ -f $privkey ] ; then
+        echo "Private key $privkey already exists!"
+        exit 1
+    fi
+    openssl req -new -newkey rsa:2048 -out req.pem -keyout $privkey -nodes -config openssl.cnf -nodes -batch
+    openssl ca -noemailDN -in req.pem -config openssl.cnf -extensions $reqext -policy policy_anything -batch
+    echo "Created certificate $CADIR/newcerts/${SERIAL}.pem"
+    echo "Private key: $CADIR/private/${SERIAL}.pem"
+    rm req.pem
 fi
 
